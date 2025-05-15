be74ded892fecaf79fa00b9307006a80b699f845
Temrario <vaninmixa02@gmail.com>
2025-05-15 18:13:28 +0300

    fix

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,4 +4,6 @@ def test_hello():
     client = app.test_client()
     response = client.get("/hello")
     assert response.status_code == 200
-    assert response.data == b"Hello, Docker!"
+    assert response.is_json
+    data = response.get_json()
+    assert data == {"message": "Hello, Docker!"}
