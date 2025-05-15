8baf38426d3b89aa171869ae66e3d97f6270d8e8
Temrario <vaninmixa02@gmail.com>
2025-05-15 17:45:20 +0300

    more

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,8 @@
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from app.main import app
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 def test_hello():
     client = app.test_client()
