4efe6eec5487e8c2f868f16859c2301136625e65
Temrario <vaninmixa02@gmail.com>
2025-05-15 17:00:33 +0300

    slack

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 
 app = Flask(__name__)
 
+
 @app.route("/hello")
 def hello():
-    return "Hello, Docker!"
+    return jsonify(message="Hello, Docker!")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
